d9b3640e36004e6108dba07e700627b47559fdf2
Jason Dugdale <jason.dugdale@gmail.com>
2017-10-30 15:49:37 -0700

    Adding Google OAuth config to Superset_config script

--- superset_config.py
+++ superset_config.py
@@ -23,3 +23,32 @@ SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
+
+#---------------------------------------------------------
+# Google OAuth configuration
+#---------------------------------------------------------
+# Following instructions from https://medium.com/@aungmt/superset-with-google-oauth-3ba7a1c1f459
+
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = 'Public'
+
+OAUTH_PROVIDERS = [
+ {
+   'name': 'google',
+   'whitelist': [os.environ['GOOGLE_OAUTH_DOMAIN']],
+   'icon': 'fa-google',
+   'token_key': 'access_token',
+   'remote_app': {
+     'base_url': 'https://www.googleapis.com/oauth2/v2/',
+     'request_token_params': {
+        'scope': 'email profile'
+     },
+     'request_token_url': None,
+     'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+     'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+     'consumer_key': os.environ['GOOGLE_OAUTH_KEY'],
+     'consumer_secret': os.environ['GOOGLE_OAUTH_SECRET']
+    }
+  }
+]
