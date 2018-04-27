fc7dea3c10994876a41261fed69045564b186834
Rômulo Madureira Rodrigues <romulo.academics@gmail.com>
2018-04-27 11:24:16 -0300

    add redis

--- superset_config.py
+++ superset_config.py
@@ -13,7 +13,14 @@ SUPERSET_WORKERS = 1 # for it to work in heroku basic/hobby dynos increase as yo
 #---------------------------------------------------------
 # Your App secret key
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
-
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 60*60*24,
+    'CACHE_KEY_PREFIX': 'superset_',
+    'CACHE_REDIS_HOST': 'redis',
+    'CACHE_REDIS_DB': 1,
+    'CACHE_REDIS_URL': os.environ['REDIS_URL']
+}
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
 # Superset metadata (slices, connections, tables, dashboards, ...).
